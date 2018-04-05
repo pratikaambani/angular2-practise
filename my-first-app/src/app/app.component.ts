5befd298781bf01968c0c8d17f9586a72b5d1b18
Pratik Ambani <pratikaambani@gmail.com>
2018-04-06 02:48:39 +0530

    more on selector component

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,12 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-custom-root',
   templateUrl: './app.component.html',
-/*  styleUrls: ['./app.component.css']*/
-  styles:[`
+  styleUrls: ['./app.component.css']
+/*  styles:[`
   	h2 {
   		color:green;
   	}
-  `]
+  `]*/
 })
 export class AppComponent {
 /*1. First change*/
