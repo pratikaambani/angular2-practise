562980af53995e51468e518c363170bfe703deb9
Pratik Ambani <pratikaambani@gmail.com>
2018-04-06 02:19:10 +0530

    inline styles

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-custom-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+/*  styleUrls: ['./app.component.css']*/
+  styles:[`
+  	h2 {
+  		color:green;
+  	}
+  `]
 })
 export class AppComponent {
 /*1. First change*/
