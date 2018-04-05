01fdd79a3304e0a49a17dfabca23c4890b8c3333
Pratik Ambani <pratikaambani@gmail.com>
2018-04-06 02:03:40 +0530

    inline template instead of external styling

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+/*  templateUrl: './servers.component.html',*/
+  template: '<app-custom-server></app-custom-server>',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
