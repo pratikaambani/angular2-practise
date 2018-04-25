dedfb449c5218bced92ad468a82ba0085bd9d504
Pratik Ambani <pratikaambani@gmail.com>
2018-04-26 00:54:23 +0530

    ngClass directive

--- my-first-app/src/app/server/server.component.ts
+++ my-first-app/src/app/server/server.component.ts
@@ -4,7 +4,12 @@ import { Component } from '@angular/core';
 @Component({
 	//html tag, should be string, should be unique
 	selector : 'app-custom-server',
-	templateUrl : './server.component.html'
+	templateUrl : './server.component.html',
+	styles: [`
+		.online {
+			color: blue;
+		}
+	`]
 })
 /*naming: NameOfComponent|Component*/
 export class ServerComponent {
