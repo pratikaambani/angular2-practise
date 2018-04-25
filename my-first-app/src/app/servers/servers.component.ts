af98f2775a0b47ad94fe8bfeb402790fa8e18326
Pratik Ambani <pratikaambani@gmail.com>
2018-04-25 22:40:13 +0530

    passing anf using data with event binding

--- my-first-app/src/app/servers/servers.component.ts
+++ my-first-app/src/app/servers/servers.component.ts
@@ -7,7 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 	allowNewServer=false;
-	serverCreationStatus="No server was created";
+    serverCreationStatus="No server was created";
+    serverName='';
 
   constructor() {
   	setTimeout(() => {
@@ -21,6 +22,11 @@ export class ServersComponent implements OnInit {
   /*naming purpose onXyzEvent*/
   onCreateServer() {
   	this.serverCreationStatus = 'Server was created';
+  }
+
+  onUpdateServer(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement>event.target).value;
   }
 
 }
