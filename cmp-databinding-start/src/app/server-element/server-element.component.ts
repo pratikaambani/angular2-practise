3e287ce9c936e0d93a9618e42c37bc7d77c1ae4b
Pratik Ambani <pratikaambani@gmail.com>
2018-05-24 11:46:13 +0530

    custom property binding using @Input, aliasing; transmitting information outside component	using @Outside

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
@@ -6,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./server-element.component.css']
 })
 export class ServerElementComponent implements OnInit {
+
+  
+  //This element is part of this component only, we can't access it outside, because by default all props of components are only accessible inside this component
+  // element: {type:string, name:string, content:string};
+
+  //Solution: Use @Input decorator, now we'll be able to expose this property to outside world
+  @Input('elementAlias') element1: {type:string, name:string, content:string};
+  //@Input('elementAlias') : Giving Alias to component
 
   constructor() { }
 
